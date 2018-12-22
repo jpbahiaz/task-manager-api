dc855e9f35eaa261681120030593fab54b26b04f
João Pedro <jp.bahia.zica@gmail.com>
2018-12-22 18:05:18 -0200

    Adding version constraints

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: "api" }, path: "/" do
 
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+    end
+
   end
 end
