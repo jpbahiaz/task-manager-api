9a94b509a23b37cd7ce04c73e488e708d307e8ea
João Pedro <jp.bahia.zica@gmail.com>
2018-12-25 20:15:13 -0200

    Adding destroy action to the users controller with tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,6 +29,12 @@ class Api::V1::UsersController < ApplicationController
         end
     end
 
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        head :no_content # 204
+    end
+
     def user_params
         params.require(:user).permit(:email, :password, :password_confirmation)
     end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -98,4 +98,19 @@ RSpec.describe 'Users API', type: :request do
             end
         end
     end
+
+    describe 'DELETE /users/:id' do
+        before do
+            headers = { 'Accept': 'application/vnd.taskmanager.v1' }
+            delete "/users/#{user_id}", headers: headers
+        end
+
+        it 'should return status code 204' do
+            expect(response).to have_http_status(204)
+        end
+
+        it 'should remove the user from database' do
+            expect(User.find_by(id: user_id)).to be_nil
+        end
+    end
 end
