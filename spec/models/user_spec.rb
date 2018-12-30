6dda911bcd5a18f3cfdfccc8117930cb877bcae9
João Pedro <jp.bahia.zica@gmail.com>
2018-12-30 19:53:37 -0200

    Adding user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe User, type: :model do
 
   let(:user) { FactoryGirl.build(:user) }
 
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
-  it { is_expected.to validate_confirmation_of(:password) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_confirmation_of(:password) }
   it { is_expected.to allow_value('jp.bahia.zica@gmail.com').for(:email)}
   it { is_expected.to validate_uniqueness_of(:auth_token) }
 
@@ -16,6 +16,25 @@ RSpec.describe User, type: :model do
       allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
 
       expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: #{Devise.friendly_token}")
+    end
+  end
+
+  describe '#generate_authentication_token!' do
+    it 'generates a unique auth token' do
+      allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+      user.generate_authentication_token!
+      
+      expect(user.auth_token).to eq('abc123xyzTOKEN')
+    end
+
+    it 'generates another auth token when the current auth token has already been taken' do
+      existing_user = FactoryGirl.create(:user, auth_token: 'abc123tokenxyz')
+      allow(Devise).to receive(:friendly_token).and_return('abc123tokenxyz', 'abcXYZ123456789')
+      user.generate_authentication_token!
+
+      expect(user.auth_token).not_to eq(existing_user.auth_token)
+
+
     end
   end
 
