adb4a158f16b88013b013ffc5584c2cf7e79e166
João Pedro <jp.bahia.zica@gmail.com>
2018-12-31 17:03:21 -0200

    Adding adssociation with tasks for user model and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
 
   let(:user) { FactoryGirl.build(:user) }
 
+  it { should have_many(:tasks).dependent(:destroy) }
+
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_confirmation_of(:password) }
