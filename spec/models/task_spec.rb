6ae2e2f9f5c95781d29671ad5a9021e6281ffd32
João Pedro <jp.bahia.zica@gmail.com>
2018-12-31 16:52:40 -0200

    Adding tests and improvements for task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:task) { FactoryGirl.build(:task) }
+
+  context 'When it is a new instance' do
+    it { expect(task).not_to be_done }
+  end
+
+  it { should belong_to(:user) }
+
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :user_id }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:description) }
+  it { should respond_to(:deadline) }
+  it { should respond_to(:done) }
+  it { should respond_to(:user_id) }
 end
